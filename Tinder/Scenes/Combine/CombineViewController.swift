ca3d90c718426532d047d959330e80ecc15689ee
claudiocarvalhodev <claudiocarvalhodev@gmail.com>
2020-02-24 09:26:55 -0800

    Add initial setup project

---
+++ Tinder/Scenes/Combine/CombineViewController.swift
@@ -0,0 +1,26 @@
+//
+//  CombineViewController.swift
+//  Tinder
+//
+//  Created by claudiocarvalho on 24/02/20.
+//  Copyright © 2020 claudiocarvalho. All rights reserved.
+//
+
+import UIKit
+
+class CombineViewController: UIViewController {
+    
+    // MARK: - View Controller Lifecycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureUI()
+    }
+    
+    // MARK: - Helper Functions
+    
+    func configureUI() {
+        view.backgroundColor = .blue
+    }
+    
+}
