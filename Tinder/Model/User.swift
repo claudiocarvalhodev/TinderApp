22b48fe55807e71515c0faa2cd15d0ea62838ddd
claudiocarvalhodev <claudiocarvalhodev@gmail.com>
2020-02-25 09:47:14 -0800

    Add UI Views and services configurations

---
+++ Tinder/Model/User.swift
@@ -0,0 +1,18 @@
+//
+//  User.swift
+//  Tinder
+//
+//  Created by claudiocarvalho on 24/02/20.
+//  Copyright © 2020 claudiocarvalho. All rights reserved.
+//
+
+import Foundation
+
+struct User {
+    let id: Int
+    let name: String
+    let age: Int
+    let match: Bool
+    let phrase: String
+    let photo: String
+}
